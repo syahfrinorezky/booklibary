229543d06e2091dfe3a1559e7a75ceff7ac9879b
rinorezky <syahfrinorezky@gmail.com>
2025-12-13 22:25:51 +0800

    chore: change put method with patch method

--- src/main/java/com/example/booklibrary/Controller/MembersController.java
+++ src/main/java/com/example/booklibrary/Controller/MembersController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -48,7 +49,7 @@ public class MembersController {
         return ResponseEntity.ok(new ApiResponse<>(true, "Member created successfully", response));
     }
 
-    @PutMapping("/{memberCode}")
+    @PatchMapping("/{memberCode}")
     public ResponseEntity<MembersRes> updateMember(@PathVariable String memberCode,
             @RequestBody @Valid MembersReq dto) {
         MembersRes updated = membersService.updateMember(memberCode, dto);
