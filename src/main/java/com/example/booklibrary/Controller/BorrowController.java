68f2d87fca4524d64978fd975568f6ff81ed1506
rinorezky <syahfrinorezky@gmail.com>
2025-12-16 06:40:31 +0800

    feat: add borrow controller

---
+++ src/main/java/com/example/booklibrary/Controller/BorrowController.java
@@ -0,0 +1,59 @@
+package com.example.booklibrary.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.booklibrary.Dto.ApiResponse.ApiResponse;
+import com.example.booklibrary.Dto.Borrows.BorrowReq;
+import com.example.booklibrary.Dto.Borrows.BorrowRes;
+import com.example.booklibrary.Dto.Borrows.ReturnReq;
+import com.example.booklibrary.Service.BorrowService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/borrows")
+public class BorrowController {
+    private final BorrowService borrowService;
+
+    public BorrowController(BorrowService borrowService) {
+        this.borrowService = borrowService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<BorrowRes>>> getAllBorrows() {
+        List<BorrowRes> borrows = borrowService.getAllBorrows();
+        return ResponseEntity.ok(new ApiResponse<>(true, "All borrow records retrieved successfully", borrows));
+    }
+
+    @GetMapping("/active")
+    public ResponseEntity<ApiResponse<List<BorrowRes>>> getActiveBorrows() {
+        List<BorrowRes> borrows = borrowService.getActiveBorrows();
+        return ResponseEntity.ok(new ApiResponse<>(true, "Active borrows retrieved successfully", borrows));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<ApiResponse<List<BorrowRes>>> getBorrowHistory() {
+        List<BorrowRes> borrows = borrowService.getBorrowHistory();
+        return ResponseEntity.ok(new ApiResponse<>(true, "Returned borrows history retrieved successfully", borrows));
+    }
+
+    @PostMapping("/borrow")
+    public ResponseEntity<ApiResponse<BorrowRes>> borrowBook(@RequestBody @Valid BorrowReq req) {
+        BorrowRes borrow = borrowService.borrowBook(req);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Book borrowed successfully", borrow));
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<ApiResponse<BorrowRes>> returnBook(@RequestBody @Valid ReturnReq req) {
+        BorrowRes borrow = borrowService.returnBook(req);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Book returned successfully", borrow));
+    }
+}
