79b7124b68ba38b4c1c86430d7e6f9132f56d188
rinorezky <syahfrinorezky@gmail.com>
2025-12-16 06:42:01 +0800

    feat: add creating category controller

--- src/main/java/com/example/booklibrary/Controller/CategoriesController.java
+++ src/main/java/com/example/booklibrary/Controller/CategoriesController.java
@@ -6,11 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.booklibrary.Dto.ApiResponse.ApiResponse;
+import com.example.booklibrary.Dto.Categories.CategoriesReq;
 import com.example.booklibrary.Dto.Categories.CategoriesRes;
 import com.example.booklibrary.Service.CategoriesService;
 
@@ -37,9 +40,15 @@ public class CategoriesController {
         return ResponseEntity.ok(new ApiResponse<>(true, "Category retrieved successfully", category));
     }
 
-    @GetMapping("/{categoryCode}")
+    @PostMapping
+    public ResponseEntity<ApiResponse<CategoriesRes>> createCategory(@RequestBody @Valid CategoriesReq dto) {
+        CategoriesRes category = categoriesService.createCategory(dto);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Category created successfully", category));
+    }
+
+    @PatchMapping("/{categoryCode}")
     public ResponseEntity<ApiResponse<CategoriesRes>> updateCategory(@PathVariable String categoryCode,
-            @RequestBody @Valid CategoriesRes dto) {
+            @RequestBody @Valid CategoriesReq dto) {
         CategoriesRes category = categoriesService.updateCategory(categoryCode, dto);
         return ResponseEntity.ok(new ApiResponse<>(true, "Category updated successfully", category));
     }
